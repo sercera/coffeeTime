1b4267c238610b2978216adff04399fed41fcf21
Milan <milanstarinac96@gmail.com>
2018-06-16 20:56:10 +0200

    pushujem da bih zipovao i sacuvao sve sto smo radili do sada

--- app/Sites/APP/Views/home.blade.php
+++ app/Sites/APP/Views/home.blade.php
@@ -39,6 +39,9 @@
 <div style="height: 30px;">
 
 </div>
+<h1 class="default-font text-bold letter-spacing-30">coffeeTime</h1>
+<br />
+<br />
 
 @foreach($caffes as $caffe)
     <div style="width: 350px; float:left; margin:25px;" >
@@ -49,7 +52,6 @@
 
 
 <script src="{{asset('FRONTEND/site/js/minified.js')}}"></script>
-
 <script src="{{asset('FRONTEND/site/js/main.js')}}"></script>
 </body>
 </html>
